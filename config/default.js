f000e3d440b02dfa1ff71e73ed30b21e6997530f
Elliot Foster <efoster@firetaco.com>
2021-09-25 16:52:32 -0500

    Extract animation into config
    So that we can tweak the animation without futzing with tests, and let
    us have simpler animations for test if we so desire.

---
+++ config/default.js
@@ -0,0 +1,67 @@
+exports.gradient_schedule = [
+  // start black
+  {
+    index: -1,
+    formula: [
+      [0,0,0,0],
+      [1,0,0,0],
+    ],
+  },
+  // purple/blue?
+  {
+    index: -0.9,
+    formula: [
+      [0,64,0,0],
+      [1,0,0,64],
+    ],
+  },
+  // red slowly fading up from bottom
+  {
+    index: -0.5,
+    formula: [
+      [0,255,0,0],
+      [1,0,0,64],
+    ],
+  },
+  // orange -> yellow
+  {
+    index: -0.25,
+    formula: [
+      [0,255,255,0],
+      [0.25,255,128,0],
+      [1,255,0,0],
+    ],
+  },
+  // full-ish yellow
+  {
+    index: -0.125,
+    formula: [
+      [0,255,255,0],
+      [1,255,128,0],
+    ],
+  },
+  // full white
+  {
+    index: 0,
+    formula: [
+      [0,255,255,255],
+      [1,255,255,255],
+    ],
+  },
+  // for a while
+  {
+    index: 0.8,
+    formula: [
+      [0,255,255,255],
+      [1,255,255,255],
+    ],
+  },
+  // fade to black
+  {
+    index: 1,
+    formula: [
+      [0,0,0,0],
+      [1,0,0,0],
+    ],
+  },
+];
---
+++ config/test.js
@@ -0,0 +1,51 @@
+exports.gradient_schedule = [
+  // start black
+  {
+    index: -1,
+    formula: [
+      [0,0,0,0],
+      [1,0,0,0],
+    ],
+  },
+  // red slowly fading up from bottom
+  {
+    index: -0.5,
+    formula: [
+      [0,255,0,0],
+      [1,0,0,0],
+    ],
+  },
+  // orange -> yellow
+  {
+    index: -0.25,
+    formula: [
+      [0,255,255,0],
+      [0.25,255,128,0],
+      [1,255,0,0],
+    ],
+  },
+  // full white
+  {
+    index: 0,
+    formula: [
+      [0,255,255,255],
+      [1,255,255,255],
+    ],
+  },
+  // for a while
+  {
+    index: 0.8,
+    formula: [
+      [0,255,255,255],
+      [1,255,255,255],
+    ],
+  },
+  // fade to black
+  {
+    index: 1,
+    formula: [
+      [0,0,0,0],
+      [1,0,0,0],
+    ],
+  },
+];
