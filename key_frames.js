187d4e129450802c18c676daaa9fad3ccfeb1999
Elliot Foster <efoster@firetaco.com>
2021-09-25 11:02:30 -0500

    Extract some of the gradient handling, key frames

---
+++ key_frames.js
@@ -0,0 +1,133 @@
+class Gradient {
+}
+
+const gradient_schedule = [
+  // start black
+  {
+    index: -1,
+    formula: [
+      [0,0,0,0],
+      [1,0,0,0],
+    ],
+  },
+  // red slowly fading up from bottom
+  {
+    index: -0.5,
+    formula: [
+      [0,255,0,0],
+      [1,0,0,0],
+    ],
+  },
+  // orange -> yellow
+  {
+    index: -0.25,
+    formula: [
+      [0,255,255,0],
+      [0.25,255,128,0],
+      [1,255,0,0],
+    ],
+  },
+  // full white
+  {
+    index: 0,
+    formula: [
+      [0,255,255,255],
+      [1,255,255,255],
+    ],
+  },
+  // fade to black
+  {
+    index: 1,
+    formula: [
+      [0,0,0,0],
+      [1,0,0,0],
+    ],
+  },
+]
+
+class KeyFrames {
+  constructor(opts) {
+    const {
+      height,
+    } = opts;
+
+    this.height = height;
+
+    this.gradient_schedule = gradient_schedule.map(({ index, formula }) => {
+      const gradient = this.gradientFrom(formula);
+      return {
+        index,
+        gradient,
+      };
+    });
+
+    this.off = {
+      from: this.gradientFrom([[0,0,0,0],[1,0,0,0]]),
+      to: this.gradientFrom([[0,0,0,0],[1,0,0,0]]),
+    };
+  }
+
+  at(t) {
+    const output = {
+      from: null,
+      to: null,
+    };
+
+    const last = this.gradient_schedule.length - 1;
+    if (t < this.gradient_schedule[0].index || t > this.gradient_schedule[last].index) {
+      return this.off;
+    }
+
+    // find two gradients that t_index fits between
+    for(let i = 0; i <= this.gradient_schedule.length -2; ++i) {
+      const from = this.gradient_schedule[i];
+      const to = this.gradient_schedule[i+1];
+
+      if (t >= from.index && t <= to.index) {
+        return {
+          from: from.gradient,
+          to: to.gradient,
+        }
+      }
+    }
+
+    // go through our gradient schedule, find the best beginning/ending key frames
+    return output;
+  }
+
+  gradientFrom(stops) {
+    if ((stops.length-1) > this.height) {
+      throw new Error(`Cannot fit ${stops.length-1} deltas into ${this.height} pixels`);
+    }
+
+    const gradient = new Array(this.height);
+
+    const channels = [0,1,2]; // R G B indexes into color array
+
+    for (let i = 0; i < stops.length - 1; ++i) {
+      const [from_pct, ...from] = stops[i];
+      const [to_pct, ...to] = stops[i+1];
+      const delta = channels.map((ch) => {
+        return to[ch] - from[ch];
+      });
+
+      const start = Math.floor(from_pct*this.height);
+      const stop = Math.ceil(to_pct*this.height);
+
+      for (let y = start; y < stop && y < this.height; ++y) {
+        const pct_up = (y-start) / Math.max(stop-start-1,1);
+
+        const color = [];
+        channels.forEach((ch) => {
+          color[ch] = Math.floor(from[ch] + delta[ch]*pct_up);
+        });
+
+        gradient[y] = color;
+      }
+    }
+
+    return gradient;
+  }
+}
+
+module.exports = KeyFrames;
---
+++ test/key_frames.js
@@ -0,0 +1,181 @@
+require('./helper');
+
+const expect = require('chai').expect;
+const KeyFrames = require('../key_frames');
+
+describe('KeyFrames', function() {
+  let inst;
+  let opts;
+
+  beforeEach(async function() {
+    opts = {
+      height: 3,
+    };
+
+    inst = new KeyFrames(opts);
+  });
+
+  describe('#at', function() {
+    context('when provided the first index', function() {
+      it('should return the first phase', async function() {
+        expect(inst.at(-1)).to.deep.equal({
+          from: [[0,0,0],[0,0,0],[0,0,0]],
+          to: [[255,0,0],[127,0,0],[0,0,0]],
+        });
+      });
+    });
+
+    context('when provided the last index', function() {
+      it('should return the last phase', async function() {
+        expect(inst.at(1)).to.deep.equal({
+          from: [[255,255,255],[255,255,255],[255,255,255]],
+          to: [[0,0,0],[0,0,0],[0,0,0]],
+        });
+      });
+    });
+
+    it('should return two gradients that surround that time (inclusive)', async function() {
+      const expected = [
+        {
+          input: -1,
+          output: {
+            from: [[0,0,0],[0,0,0],[0,0,0]],
+            to: [[255,0,0],[127,0,0],[0,0,0]],
+          },
+        },
+        {
+          input: -0.99,
+          output: {
+            from: [[0,0,0],[0,0,0],[0,0,0]],
+            to: [[255,0,0],[127,0,0],[0,0,0]],
+          },
+        },
+        {
+          input: 0.01,
+          output: {
+            from: [[255,255,255],[255,255,255],[255,255,255]],
+            to: [[0,0,0],[0,0,0],[0,0,0]],
+          },
+        },
+        {
+          input: 0.99,
+          output: {
+            from: [[255,255,255],[255,255,255],[255,255,255]],
+            to: [[0,0,0],[0,0,0],[0,0,0]],
+          },
+        },
+        {
+          input: 1,
+          output: {
+            from: [[255,255,255],[255,255,255],[255,255,255]],
+            to: [[0,0,0],[0,0,0],[0,0,0]],
+          },
+        },
+      ];
+
+      const actual = [];
+
+      expected.forEach(function({ input }) {
+        actual.push({
+          input,
+          output: inst.at(input),
+        });
+      });
+
+      expect(actual).to.deep.equal(expected);
+    });
+
+    context('when the time index that is provided is outside -1 and 1', function() {
+      it('should entirely off gradients', async function() {
+        const expected = [
+          {
+            input: -100,
+            output: {
+              from: [[0,0,0],[0,0,0],[0,0,0]],
+              to: [[0,0,0],[0,0,0],[0,0,0]],
+            },
+          },
+          {
+            input: -1.01,
+            output: {
+              from: [[0,0,0],[0,0,0],[0,0,0]],
+              to: [[0,0,0],[0,0,0],[0,0,0]],
+            },
+          },
+          {
+            input: 1.01,
+            output: {
+              from: [[0,0,0],[0,0,0],[0,0,0]],
+              to: [[0,0,0],[0,0,0],[0,0,0]],
+            },
+          },
+          {
+            input: 100,
+            output: {
+              from: [[0,0,0],[0,0,0],[0,0,0]],
+              to: [[0,0,0],[0,0,0],[0,0,0]],
+            },
+          },
+        ];
+
+        const actual = [];
+
+        expected.forEach(function({ input }) {
+          actual.push({
+            input,
+            output: inst.at(input),
+          });
+        });
+
+        expect(actual).to.deep.equal(expected);
+      });
+    });
+  });
+
+  describe('#gradientFrom', function() {
+    it('should return a gradient from the description', async function() {
+      expect(inst.gradientFrom([[0,0,0,0],[1,255,0,0]])).to.deep.equal([
+        [0,0,0],
+        [127,0,0],
+        [255,0,0],
+      ]);
+    });
+
+    context('when more than one stop is provided', function() {
+      it('should interpolate correctly', async function() {
+        opts.height = 5;
+        const inst = new KeyFrames(opts);
+        expect(inst.gradientFrom([[0,255,0,0],[0.5,1,0,0],[1,3,0,0]])).to.deep.equal([
+          [255,0,0],
+          [128,0,0],
+          [1,0,0],
+          [2,0,0],
+          [3,0,0],
+        ]);
+      });
+    });
+
+    context('when a stop is provided for each pixel in the Y', function() {
+      beforeEach(async function() {
+        opts.height = 2;
+        inst = new KeyFrames(opts);
+      });
+
+      it('should use those stops as the pixel values', async function() {
+        expect(inst.gradientFrom([[0,255,0,0],[0.5,0,0,0],[1,0,0,0]])).to.deep.equal([
+          [255,0,0],
+          [0,0,0],
+        ]);
+      });
+    });
+
+    context('when more stops are provided than there are pixels', function() {
+      it('should throw', async function() {
+        inst = new KeyFrames(opts);
+        expect(function() {
+          inst.gradientFrom([[0,255,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]);
+        }).to.throw('Cannot fit 4 deltas into 3 pixels');
+      });
+    });
+  });
+});
